de0740a67c6592d683bab21559610bfd69fb633d
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-07-14 23:27:10 +0300

    Mvc projeye eklendi..

---
+++ UdemyNLayerProject.Web/Dtos/ProductDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Models;
+
+namespace UdemyNLayerProject.Web.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="{0} alanı gereklidir.")]
+        public string Name { get; set; }
+        [Range(1,maximum:int.MaxValue,ErrorMessage ="{0} birden büyük gereklidir")]
+        
+        public int Stock { get; set; }
+        [Range(0, maximum: double.MaxValue, ErrorMessage = "{0} 0 den büyük olmalı büyük gereklidir")]
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+      
+
+
+
+    }
+}
